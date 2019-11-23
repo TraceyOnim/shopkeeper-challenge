bb278ad0703a0ce62815cbbb83c61dcf444073e7
Tee22 <traceyOnim22@gmail.com>
2019-11-23 16:56:24 +0300

    document functions

--- level1.ex
+++ level1.ex
@@ -1,8 +1,18 @@
 defmodule ShopkeeperChallenge do
+  @doc """
+  takes in the change and returns the amount in denominations
+  """
   def call_me(change) do
     return_change(change, "")
   end
 
+  @doc """
+  takes in the change and acc which is the remainder we get by dividing the change with denomination.
+  Returns a string.
+   ## Example
+   iex > ShopKeeperChallenge.return_change(94)
+        "1 50 Notes 2 20 Coins 4 1 Coin "
+  """
   def return_change(0, acc) do
     acc
   end
@@ -12,35 +22,35 @@ defmodule ShopkeeperChallenge do
     return_change(remaining, acc <> string)
   end
 
-  def change_amount(change) when change > 1000 do
+  defp change_amount(change) when change > 1000 do
     {"#{div(change, 1000)} 1000 Notes ", rem(change, 1000)}
   end
 
-  def change_amount(change) when change in 500..1000 do
+  defp change_amount(change) when change in 500..1000 do
     {"#{div(change, 500)} 500 Notes ", rem(change, 500)}
   end
 
-  def change_amount(change) when change in 100..500 do
+  defp change_amount(change) when change in 100..500 do
     {"#{div(change, 100)} 100 Notes ", rem(change, 100)}
   end
 
-  def change_amount(change) when change in 50..100 do
+  defp change_amount(change) when change in 50..100 do
     {"#{div(change, 50)} 50 Notes ", rem(change, 50)}
   end
 
-  def change_amount(change) when change in 20..50 do
+  defp change_amount(change) when change in 20..50 do
     {"#{div(change, 20)} 20 Coins ", rem(change, 20)}
   end
 
-  def change_amount(change) when change in 10..20 do
+  defp change_amount(change) when change in 10..20 do
     {"#{div(change, 10)} 10 Coins ", rem(change, 10)}
   end
 
-  def change_amount(change) when change in 5..10 do
+  defp change_amount(change) when change in 5..10 do
     {"#{div(change, 5)} 5 Coins ", rem(change, 5)}
   end
 
-  def change_amount(change) when change in 1..5 do
+  defp change_amount(change) when change in 1..5 do
     {"#{div(change, 1)} 1 Coin ", rem(change, 1)}
   end
 end
--- level2.ex
+++ level2.ex
@@ -10,6 +10,10 @@ defmodule ShopKeeper do
     "Towel" => 236,
     "Soda" => 65
   }
+
+  @doc """
+  returns a list of available items
+  """
   def available_items do
     Enum.map(@item_prices, fn {item, price} ->
       {item, price}
@@ -21,7 +25,11 @@ defmodule ShopKeeper do
     IO.gets("")
   end
 
-  def bill(items) do
+  @doc """
+  given a list of items, this function prompts the customer to pay the total price
+  of their items and returns the balance in form of denominations.
+  """
+  def enter_items_to_get_bill(items) do
     total =
       Enum.map(items, fn item ->
         price(item)
@@ -31,18 +39,27 @@ defmodule ShopKeeper do
     total
     |> _prompt()
     |> balance(total)
-    |> enter_bill()
+    |> balance_in_denominations()
   end
 
+  @doc """
+  takes in the price paid by the customer and returns the balance
+  """
   def balance(paid, total) do
     paid = String.replace(paid, "\n", "") |> String.to_integer()
     paid - total
   end
 
-  def enter_bill(bill) do
+  @doc """
+  takes in the balance and returns the balance in form of denominations
+  """
+  def balance_in_denominations(bill) do
     ShopkeeperChallenge.call_me(bill)
   end
 
+  @doc """
+  returns the price of the item given
+  """
   def price(item) do
     item = String.capitalize(item)
     @item_prices[item]
